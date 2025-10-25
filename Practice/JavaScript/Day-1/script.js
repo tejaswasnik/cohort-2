e5f65f7d2343445b950ce57351294be7644807a3
Tejas Wasnik <tejaswasnik@icloud.com>
2025-10-25 18:38:35 +0530

    JavaScript Day 1 Completed

---
+++ Practice/JavaScript/Day-1/script.js
@@ -0,0 +1,17 @@
+console.warn("warn");
+console.log("log");
+console.error("error");
+alert("this is an alert");
+prompt("name ?")
+
+let str1 = "Harsh bhai kaise ho";
+let sliced = str1.slice(3,9);
+
+alert(sliced);
+var a = 1;
+let b = 6;
+const c = 7;
+
+a = 2;
+b = 8;
+c = 99;
