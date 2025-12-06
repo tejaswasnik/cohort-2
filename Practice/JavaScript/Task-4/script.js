2ed688d8853247186dcbbfb43a3511727713715c
Tejas Wasnik <tejaswasnik@icloud.com>
2025-12-06 15:00:03 +0530

    made some progress

--- Practice/JavaScript/Task-4/script.js
+++ Practice/JavaScript/Task-4/script.js
@@ -57,32 +57,100 @@ Outer();
 
 /*  7. Create an array of 5 fruits. Add one at the end and
  remove one from the beginning.*/
- let fruits = ["apple", "mango", "banana", "kiwi"];
-    fruits.push("papaya");
-    fruits.shift();
-    console.log(fruits)
+let fruits = ["apple", "mango", "banana", "kiwi"];
+fruits.push("papaya");
+fruits.shift();
+console.log(fruits);
 
- /* 8. Use a `for` loop to print all elements of an array. */
- let arr1 = [1,2,3,4,5,6,7,8,9]
- for(let i = 0; i < arr1.length; i++) {
-    console.log(arr1[i]);
- }
+/* 8. Use a `for` loop to print all elements of an array. */
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < arr1.length; i++) {
+  console.log(arr1[i]);
+}
 
- /* 9. Create an object `person` with keys `name`, `age`,
+/* 9. Create an object `person` with keys `name`, `age`,
  and `city`, and print each key’s value.*/
- let person = {
-    name : "Tejas",
-    age : 21,
-    city : "Mumbai"
- }
+let person = {
+  name: "Tejas",
+  age: 21,
+  city: "Mumbai",
+};
 
- for(let i in person) {
-    console.log(person[i]);
- }
+for (let i in person) {
+  console.log(person[i]);
+}
 
- /*
+/*
  10. Use `setTimeout()` to log `"Time’s up!"` after 2
  seconds.*/
-setTimeout(function() {
-    console.log(`Time's up`);
-}, 2000)
+setTimeout(function () {
+  console.log(`Time's up`);
+}, 2000);
+
+// Level 2
+
+/* 1. Write a higher-order function `runTwice(fn)` that
+takes another function and executes it two times.*/
+function runTwice(fn) {
+  fn();
+  fn();
+}
+
+runTwice(function () {
+  console.log("Hello");
+});
+
+/* 2. Create one pure function that always returns the
+same output for a given input, and one impure
+function using a global variable.*/
+
+function pure(a, b) {
+  console.log(a + b);
+}
+pure(2, 5);
+
+let x = 0;
+function impure(a) {
+  x++;
+  console.log(a + x);
+}
+
+impure(3);
+impure(5);
+
+/*
+3. Write a function that uses object destructuring
+inside parameters to extract and print `name` and
+`age`.
+*/
+
+/*
+4. Demonstrate the difference between normal
+function and arrow function when used as object
+methods (the `this` issue). */
+
+/*
+5. Given an array of numbers, use `map()` to create a
+new array where each number is squared.
+*/
+/*
+6. Use `filter()` to get only even numbers from an
+array.
+*/
+/*
+7. Use `reduce()` to find the total salary from an array
+of numbers `[1000, 2000, 3000]`.
+*/
+/*
+8. Create an array of names and use `some()` and
+`every()` to test a condition (e.g., all names longer than
+3 chars).
+*/
+/*
+9. Create an object `user` and test the behavior of
+`Object.freeze()` and `Object.seal()` by
+adding/changing keys.
+*/
+/*
+10. Create a nested object (`user → address → city`) and
+access the city name inside it.*/
