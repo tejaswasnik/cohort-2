6d4dea6d004a8d3a2671e09b56ef2d780445e201
Tejas Wasnik <tejaswasnik@icloud.com>
2025-12-09 18:03:15 +0530

    made changes

--- Projects/Task-16/script.js
+++ Projects/Task-16/script.js
@@ -3,19 +3,18 @@ var btn = document.querySelector("button");
 var inner = document.querySelector(".inner");
 var h1 = document.querySelector("h1");
 var a = 0;
+
 btn.addEventListener("click", function () {
-  var num = 50 + Math.floor(Math.random() * 100);
   var x = setInterval(function () {
     a++;
     inner.style.width = a + "%";
     h1.innerHTML = a + "%";
-    console.log(a)
-  },1);
-  setTimeout(function () {
-    clearInterval(x);
-    btn.innerHTML = "Downloaded";
-    btn.style.opacity = 0.5;
-  }, 1525);
-  console.log(num);
+    if (a === 100) {
+      clearInterval(x);
+      btn.innerHTML = "Downloaded";
+      btn.style.opacity = 0.5;
+    }
+  }, 10);
+
   btn.style.pointerEvents = "none";
 });
