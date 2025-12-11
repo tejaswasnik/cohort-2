b539d4617de37549facc1f9306b2b068d33a8434
Tejas Wasnik <tejaswasnik@icloud.com>
2025-12-11 11:36:25 +0530

    fixed some things

--- Projects/Task-17/script.js
+++ Projects/Task-17/script.js
@@ -5,8 +5,11 @@ img.addEventListener('dblclick', function() {
     heart.style.opacity = 1;
     heart.style.transform = "translate(-50%,-50%) scale(1) rotate(0deg)";
     setTimeout(function() {
-        console.log('chal gya')
             heart.style.transform = "translate(-50%,-500%) scale(1) rotate(0deg)";
+     }, 600)
+    setTimeout(function() {
+        heart.style.opacity = 0;   
+        heart.style.transform = "translate(-50%,-50%) scale(0) rotate(-90deg)";
      }, 1000)
     console.log("clicked")
 })
